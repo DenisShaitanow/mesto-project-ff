b693645bd296ab5d8364b3bed677bc0766ac2a92
DenisShaitanow <shaitanow@yandex.ru>
2025-01-15 17:41:26 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,48 @@
 // @todo: Темплейт карточки
 
+const template=document.querySelector('#card-template');
+
 // @todo: DOM узлы
+
+const deleteButton=document.querySelectorAll('.card__delete-button');
+const placesList=document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function makeCard (name, link, funcDelBut) {
+    const cloneNode=template.cloneNode(true);
+    const cardImage=cloneNode.querySelector('.card__image');
+    cardImage.alt=name;
+    cardImage.src=link;
+    const cardTitle=cloneNode.querySelector('.card__title');
+    cardTitle.textContent=name;
+    
+    
+    deleteButton.add.EventListener('click', funcDelBut());
+    return cloneNode;
+
+};
+
 // @todo: Функция удаления карточки
 
+function funcDelBut() {
+    
+    const cardItem=deleteButton.closest('.places__item card');
+    cardItem.remove();
+    
+}
+
+
 // @todo: Вывести карточки на страницу
+
+
+for (let i=0; i <=initialCards.length;  i+=1) {
+    const c=makeCard(initialCards[i]['name'], initialCards[i]['link'], funcDelBut);
+     console.log(c);
+    placesList.append(c);
+ };
+ 
+
+
+
+
